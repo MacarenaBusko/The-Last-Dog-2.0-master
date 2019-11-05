5f88601f6e29aa13d7cc254b0960e42d4e80505e
MacarenaBusko <57376131+MacarenaBusko@users.noreply.github.com>
2019-11-04 23:31:40 -0300

    Add files via upload

---
+++ The-Last-Dog-2.0-master/Assets/Scripts/cinemáticas.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
+
+public class VideoPlay : MonoBehaviour {    
+
+     private UnityEngine.Video.VideoPlayer videoPlayer;
+     private string status;
+     private string sceneName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;    
+        
+
+        switch (sceneName) { 
+            case "Escena2":
+                videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath,"Cinematica-intro.mp4");
+            break;
+            
+
+
+         
+                  videoPlayer.Play();        
+        
+    }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
