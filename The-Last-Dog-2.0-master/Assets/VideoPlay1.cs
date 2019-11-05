ee468514140de56b4cff0661f5d74a5f7f596e41
MacaBusko <31540177+MacaBusko@users.noreply.github.com>
2019-11-04 21:49:25 -0300

    VideoPlay1 script
    Actalización SCRIPT

---
+++ The-Last-Dog-2.0-master/Assets/VideoPlay1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
+
+
+public class VideoPlay1 : MonoBehaviour
+{
+      public static VideoPlayer videoInicial;
+    bool empezar = false;
+    private string sceneName;
+    public GameObject camAnimacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;    
+        videoInicial = GameObject.Find("Video Player").GetComponent<VideoPlayer>();        
+         
+      
+
+        switch (sceneName) { 
+
+          case "Cinematica1":
+                 videoInicial.url = Path.Combine(Application.streamingAssetsPath, "Cinematica_intro.mp4");
+break; 
+
+            case "Cinematica2":
+                 videoInicial.url = Path.Combine(Application.streamingAssetsPath, "BosqueInicio.mp4");
+              
+               
+                break;
+
+               case "Cinematica3":
+                 videoInicial.url = Path.Combine(Application.streamingAssetsPath, "Bosque_ciudad_boceto.mp4");
+              
+               
+                break;   
+
+                 case "Cinematica4":
+                 videoInicial.url = Path.Combine(Application.streamingAssetsPath, "Cinematica-comienzo-fabrica.mp4");
+break; 
+            }
+                
+        videoInicial.Play();        
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
